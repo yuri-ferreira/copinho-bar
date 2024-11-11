f0ede19dc6d2d80940402aaa34154a63eb8aa87d
Yuri Brigido Ferreira <yuri.pipico@gmail.com>
2024-11-11 15:20:56 -0300

    back-end

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const botaoP = document.getElementById("bPromo");
-botaoP.addEventListener('click',promoExtPromo)
-
-function promoExtPromo(){
-  document.getElementById("filtru").classList.toggle("teste")
-  document.getElementById("filtroPA").classList.toggle("filtros-promo-a")
-}
